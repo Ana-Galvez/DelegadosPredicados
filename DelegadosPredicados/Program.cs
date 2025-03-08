f34b1c7db3013bef076e4ccae6707eb621a1e186
Ana Galvez <anamariagalvez01@gmail.com>
2025-03-08 03:54:53 -0300

    Agregar archivos de proyecto.

---
+++ DelegadosPredicados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegadosPredicados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
