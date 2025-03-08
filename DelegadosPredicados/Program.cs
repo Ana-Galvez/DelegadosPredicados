33ab35ce47428c9c09aa424380775a60af4542ff
Ana Galvez <anamariagalvez01@gmail.com>
2025-03-08 04:16:11 -0300

    feat: delegado predicado 1

--- DelegadosPredicados/Program.cs
+++ DelegadosPredicados/Program.cs
@@ -7,6 +7,16 @@ namespace DelegadosPredicados
     {
         static void Main(string[] args)
         {
+            List<int> numeros = new List<int>( new int[] {1,2,3,4,5,6,7,8,9,10});
+            Predicate<int> PredicadoPares = new Predicate<int>(MostrarPares);
+            List<int> numerosPares = numeros.FindAll( PredicadoPares );
+            foreach (var item in numerosPares) Console.WriteLine(item);
+        }
+
+        static bool MostrarPares(int numero)
+        {
+            if (numero % 2 == 0) return true;
+            else return false;
         }
     }
 }
